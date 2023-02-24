25e65ea72f2a8b485e39f3bc81160d0322e01ca9
Akuma <86250671+rep-aku@users.noreply.github.com>
2023-02-24 23:20:46 +0100

    Push_swapp

--- src/push.c
+++ src/push.c
@@ -6,50 +6,35 @@
 /*   By: hahadiou <hahadiou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 21:21:50 by hahadiou          #+#    #+#             */
-/*   Updated: 2023/02/24 23:14:40 by hahadiou         ###   ########.fr       */
+/*   Updated: 2023/02/24 23:20:38 by hahadiou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
 
+
 void	pa(t_stacks *stack)
 {
-	int	temp;
 	int	i;
 
-	if (stack->size_b)
-	{
-		temp = stack->b[0];
-		i = 0;
-		while (++i < stack->size_b)
-			stack->b[i - 1] = stack->b[i];
-		--stack->size_b;
-		i = stack->size_a + 1 + (stack->size_a == 0);
-		while (--i)
-			stack->a[i] = stack->a[i - 1];
-		stack->a[0] = temp;
-		++stack->a;
-		printf("pa\n");
-	}
+	stack->size_a++;
+	stack->a[0] = stack->b[0];
+	i = -1;
+	stack->size_b--;
+	while (++i < stack->size_b)
+		stack->b[i] = stack->b[i + 1];
+	printf("pa\n");
 }
 
 void	pb(t_stacks *stack)
 {
-	int	temp;
 	int	i;
 
-	if (stack->size_a)
-	{
-		temp = stack->a[0];
-		i = 0;
-		while (++i < stack->size_a)
-			stack->a[i - 1] = stack->a[i];
-		--stack->size_a;
-		i = stack->size_b + 1 + (stack->size_b == 0);
-		while (--i)
-			stack->b[i] = stack->b[i - 1];
-		stack->b[0] = temp;
-		++stack->size_b;
-		printf("pb\n");
-	}
+	stack->size_b++;
+	stack->b[0] = stack->a[0];
+	i = -1;
+	stack->size_a--;
+	while (++i < stack->size_a)
+		stack->a[i] = stack->a[i + 1];
+	printf("pb\n");
 }
--- src/rotate.c
+++ src/rotate.c
@@ -6,7 +6,7 @@
 /*   By: hahadiou <hahadiou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 21:22:23 by hahadiou          #+#    #+#             */
-/*   Updated: 2023/02/24 23:15:21 by hahadiou         ###   ########.fr       */
+/*   Updated: 2023/02/20 19:24:49 by hahadiou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,25 @@
 
 void	ra(t_stacks *stack)
 {
-	int	temp;
-	int	i;
+	int	tmp;
 
-	if (stack->size_a > 1)
-	{
-		temp = stack->a[0];
-		i = 0;
-		while (++i < stack->size_a)
-			stack->a[i - 1] = stack->a[i];
-		stack->a[stack->size_a - 1] = temp;
-		printf("ra\n");
-	}
+	tmp = stack->a[stack->size_a - 1];
+	for (int i = stack->size_a - 1; i > 0; i--)
+		stack->a[i] = stack->a[i - 1];
+	stack->a[0] = tmp;
+	printf("ra\n");
 }
 
 void	rb(t_stacks *stack)
 {
-	int	temp;
-	int	i;
+	int	tmp;
 
-	if (stack->size_b > 1)
-	{
-		temp = stack->b[0];
-		i = 0;
-		while (++i < stack->size_b)
-			stack->b[i - 1] = stack->b[i];
-		stack->b[stack->size_b - 1] = temp;
-		printf("rb\n");
-	}
+	tmp = stack->a[stack->size_a - 1];
+	for (int i = stack->size_a - 1; i > 0; i--)
+		stack->a[i] = stack->a[i - 1];
+	stack->a[0] = tmp;
+	printf("rb\n");
 }
-
 
 void	rr(t_stacks *stack)
 {
@@ -78,19 +67,15 @@ void	rra(t_stacks *stack)
 
 void	rrb(t_stacks *stack)
 {
-	int	temp;
-	int	i;
+	int	tmp;
 
-	if (stack->size_b > 1)
-	{
-		temp = stack->b[stack->size_b - 1];
-		i = stack->size_b;
-		while (--i)
-			stack->b[i] = stack->b[i - 1];
-		stack->b[0] = temp;
-		printf("rrb\n");
-	}
+	tmp = stack->b[stack->size_b - 1];
+	for (int i = stack->size_b - 1; i > 0; i--)
+		stack->b[i] = stack->b[i - 1];
+	stack->b[0] = tmp;
+	printf("rrb\n");
 }
+
 void	rrr(t_stacks *stack)
 {
 	int	tmp_a;
