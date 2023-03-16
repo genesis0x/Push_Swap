508651cf4b469b382c65903a041530b3c1175aa9
Akuma <86250671+rep-aku@users.noreply.github.com>
2023-03-16 17:14:46 +0100

    Fix Some Parsing Errors

--- mandatory/push_swap.c
+++ mandatory/push_swap.c
@@ -6,11 +6,17 @@
 /*   By: hahadiou <hahadiou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 15:59:12 by hahadiou          #+#    #+#             */
-/*   Updated: 2023/03/14 22:41:26 by hahadiou         ###   ########.fr       */
+/*   Updated: 2023/03/16 17:14:16 by hahadiou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static void	error(void)
+{
+	ft_puts("Error");
+	exit(1);
+}
 
 int	main(int ac, char **av)
 {
@@ -24,6 +30,8 @@ int	main(int ac, char **av)
 		i = 1;
 		while (av[i])
 		{
+			if (!av[i][0])
+				error();
 			temp = ft_strjoin(temp, av[i]);
 			temp = ft_strjoin(temp, " ");
 			i++;
